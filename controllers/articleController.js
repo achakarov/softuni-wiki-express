a6118088141f5975a7819162f0dd11117e71e1b7
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-18 19:52:58 +0200

    Create article working

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const Article = require('../models/Article');
 
 router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.get('/')
+
+
+
+router.post('/create', (req, res, next) => {
+    const { title, description } = req.body;
+    Article.create({title, description, author:res.user._id})
+        .then((createdArticle) => {
+            res.redirect('/');
+        })
+        .catch(next);
+});
 
 module.exports = router;
