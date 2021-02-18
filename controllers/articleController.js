9b0112b138032f538b71800c556ef1867e1049d5
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-18 19:59:03 +0200

    created proper errors when creating article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -10,6 +10,14 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res, next) => {
     const { title, description } = req.body;
+    if (title.length < 5) {
+        return next({ message: 'Title must be at least 5 characters long', status: 400 });
+    }
+
+    if (description.length < 20) {
+        return next({ message: 'Description must be at least 20 characters long', status: 400 });
+    }
+    
     Article.create({title, description, author:res.user._id})
         .then((createdArticle) => {
             res.redirect('/');
